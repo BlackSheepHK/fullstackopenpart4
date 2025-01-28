6575826fb9fe503bee81f22e08256ffbfa9ba4b3
KaLong Chan <kalongchan@KaLongs-MacBook-Pro.local>
2025-01-28 22:11:46 +0800

    Exercise 4.1-4.2

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const cors = require('cors')
+const mongoose = require("mongoose");
+const blogRouter = require("./controllers/blogRouter");
+const config = require("./utils/config");
+
+app.use(cors());
+app.use(express.json());
+app.use("/api/blogs", blogRouter);
+
+console.log("Connecting", config.MONGODB_URI);
+mongoose.connect(config.MONGODB_URI);
+console.log("Connection successful");
+
+module.exports = app;
